9bc66b470d3243623ab5d9a14b11dc4f54edd857
talweller123-glitch <talweller123@gmail.com>
2026-01-06 18:18:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from skimage.filters import median
-from skimage.morphology import ball
-import numpy as np
-from PIL import Image
-
-clean_image = median(edgeMAG, ball(3)) 
-
-# Normalize and convert to uint8 for saving as PNG
-clean_image_scaled = 255 * (clean_image - clean_image.min()) / (clean_image.max() - clean_image.min())
-clean_image_uint8 = clean_image_scaled.astype(np.uint8)
-
-edge_image = Image.fromarray(clean_image_uint8)
-edge_image.save('my_edges1.png')
 
