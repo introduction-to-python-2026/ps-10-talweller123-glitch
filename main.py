faf9e11fc0c4f4f75ee770854764dbe5dcce5b73
talweller123-glitch <talweller123@gmail.com>
2026-01-06 18:14:44 +0200

    Update main.py2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from skimage.filters import median
-from skimage.morphology import disk # Changed from ball to disk
+from skimage.morphology import ball
 import numpy as np
 from PIL import Image
 
-clean_image = median(edgeMAG, disk(3)) # Changed ball(3) to disk(3)
+clean_image = median(edgeMAG, ball(3)) 
 
 # Normalize and convert to uint8 for saving as PNG
 clean_image_scaled = 255 * (clean_image - clean_image.min()) / (clean_image.max() - clean_image.min())
