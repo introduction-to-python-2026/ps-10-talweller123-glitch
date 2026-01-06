21de24fb47037c826e9bb0a7410700c3754a5c99
talweller123-glitch <talweller123@gmail.com>
2026-01-06 18:52:32 +0200

    Update image_utils.py1

--- image_utils.py
+++ image_utils.py
@@ -1,1 +1,29 @@
+import numpy as np
+from PIL import Image
+from scipy.signal import convolve2d
 
+def load_image(image_path):
+    img = Image.open(image_path)
+    img = img.convert('RGB')
+    return np.array(img)
+
+def edge_detection(image_array):
+    gray_image = np.mean(image_array, axis=2)
+    
+    kernelY = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+    
+    kernelX = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ])
+    
+    edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+    edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG
